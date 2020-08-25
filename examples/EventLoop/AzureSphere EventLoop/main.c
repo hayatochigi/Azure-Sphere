fb51902fe435f547863c584b144cced2aff91d1f
hayatochigi <hayatochigi.shimane.pre117@gmail.com>
2020-08-25 17:35:38 +0900

    Rename Folder

---
+++ examples/EventLoop/AzureSphere EventLoop/main.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+// NULL is defined in "stdio.h"
+#include <applibs/log.h>
+#include <applibs/eventloop.h>
+#include <sys/timerfd.h>
+
+int LogWrite(void);
+
+int main(void)
+{
+    static EventLoop *myEvent;
+    struct timespec polling_t = { .tv_sec = 5, .tv_nsec = 0 };
+
+    myEvent = EventLoop_Create();
+    if (myEvent == NULL) {
+        Log_Debug("Error in EventLoop_Create");
+    }
+
+    EventLoop_RegisterIo(myEvent, polling_t, LogWrite);
+    
+
+    EventLoop_Close(myEvent);
+
+    return 0;
+}
+
+int LogWrite() {
+    return 0;
+}
