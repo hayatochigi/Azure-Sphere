eecab206060e9d809d7cddcc9c52969bdc5beda2
Yusuke Tochigi <hayatochigi.shimane.pre117@gmail.com>
2020-11-01 06:37:40 +0900

    New Commit -FileUpload

---
+++ Azure IoT Hub/FileUpload/FileUpload/FileUpload/main.c
@@ -0,0 +1,28 @@
+﻿#include <applibs/log.h>
+#include <applibs/storage.h>
+#include "iothub_device_client_ll.h"
+
+#include <unistd.h>
+
+int main(void)
+{
+    int fileFd;
+
+    // 新規作成または既存ファイルを開き、fdを取得
+    fileFd = Storage_OpenMutableFile();
+    if (fileFd == -1) {
+        Log_Debug("Error at Storage_OpenMutableFile\n");
+    }
+
+    // 書き込みデータ
+    char wMessage[] = "Hi from Azure Sphere\n";
+    ssize_t wSize = write(fileFd, wMessage, sizeof(wMessage));
+    if (wSize == -1) {
+        Log_Debug("Error at write\n");
+    }
+    
+    close(fileFd);
+
+
+    return 0;
+}
